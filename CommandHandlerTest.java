c361b997f16f48b43dfb3135ecbacd3236642db7
unknown <jdp02u@TANZANIA.cs.ad.nottingham.ac.uk>
2015-01-18 14:58:36 +0000

    JUnit tests for Command Handler all completed and passed successfully Signed-off-by: unknown <jdp02u@TANZANIA.cs.ad.nottingham.ac.uk>

--- CommandHandlerTest.java
+++ CommandHandlerTest.java
@@ -9,6 +9,12 @@ public class CommandHandlerTest {
 	public void testhandleInput() {
 		CommandHandler ch = new CommandHandler();
 		assertEquals("Error, invalid input",ch.handleInput(""));
+		assertEquals("stat",ch.handleInput("STAT"));
+		assertEquals("identify",ch.handleInput("IDEN"));
+		assertEquals("list in handler",ch.handleInput("LIST"));
+		assertEquals("message",ch.handleInput("MESG"));
+		assertEquals("broadcast",ch.handleInput("HAIL"));
+		assertEquals("+OK client signing out",ch.handleInput("QUIT"));
 	}
 
 }
