5a68e649097eb8cc2baf6d3bf8dd8a60d0b1072d
unknown <ljp02u@THAILAND.cs.ad.nottingham.ac.uk>
2015-01-18 14:25:22 +0000

    updated again Signed-off-by: unknown <ljp02u@THAILAND.cs.ad.nottingham.ac.uk>

--- ChatThread.java
+++ ChatThread.java
@@ -3,7 +3,6 @@ import java.io.*;
 
 public class ChatThread extends Thread {
 	private Socket socket = null;
-	//private String nameInput = null;
 
 	
 	public ChatThread(Socket svrSocket){
@@ -20,24 +19,23 @@ public class ChatThread extends Thread {
 			CommandHandler ch = new CommandHandler();
 			String output;
 			String input;
-			//String name;
-			serverOutput.println("Please enter a username");
+			String name;
+			serverOutput.println("Would you like to sign in with a name? (y/n)");
+			input = clientInput.readLine();
+			if(input.equals("y") || input.equals("Y")){
+				serverOutput.println("Please enter a username");
+				name = clientInput.readLine();
+				serverOutput.println("Welcome " + name);
+				//create method to add name to list of users
+			} else{
+				serverOutput.println("You will continue not logged in, some functions will not be available");
+			}
 			while((input = clientInput.readLine()) != null){
-				/*while(nameInput == null){
-
-					nameInput = clientInput.readLine();
-					if(nameInput == null){
-						return;
-					} else {
-						name = nameInput;
-						serverOutput.println("Welcome " + name);
-					}*/
-				
 				output = ch.handleInput(input);
-				serverOutput.println(output);
 				if(output.contains("+OK client signing out")){
 					break;
 				}
+				serverOutput.println(output);				
 			}
 			socket.close();	
 			System.out.println("User has logged out");
