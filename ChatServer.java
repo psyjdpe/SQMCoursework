4221e42d7bf755cd94c0dc28b674a198ed8497ac
unknown <ljp02u@BHUTAN.cs.ad.nottingham.ac.uk>
2015-01-19 13:45:08 +0000

    Finally managed to get users working, removed clientinfo and clienthandler classes Signed-off-by: unknown <ljp02u@BHUTAN.cs.ad.nottingham.ac.uk>

--- ChatServer.java
+++ ChatServer.java
@@ -1,45 +1,61 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class ChatServer{
+	
+	private static int timeout = 10000;
+	private static int port;
+	ServerSocket svrSocket = null;
+	
+	protected static Hashtable<String, Socket> online = new Hashtable<String, Socket>();
+	
+	public ChatServer(int port) throws IOException{
+		waiting(port);
+	}
+	
+	
 	public static void main(String[] args) throws IOException {
 	
-		int timeout = 10000;
-		int port;
-		boolean waiting = true;
-		ServerSocket svrSocket = null;
-		
-		
+		port = Integer.parseInt(args[0]);
 		if(args.length < 1){
 			System.err.println("No port number specified, please try again");
 			System.exit(1);
 		}
-		
-		port = Integer.parseInt(args[0]);
-		
-		try{
-			svrSocket = new ServerSocket(port);
-			Socket userSocket;
-			while(waiting){
-				userSocket = svrSocket.accept();
-				userSocket.setSoTimeout(timeout);
-				System.out.println("Client accepted " + userSocket);
-				new ChatThread(userSocket).start();
-				
-			}
-		}
-		catch (IOException ioe){
-			ioe.printStackTrace();
-			System.exit(-1);
-		}
-		finally{
-			svrSocket.close();
-		}
-		
+		System.out.println("Server started on port: " + port + " waiting for clients...");
+		new ChatServer(port);
 	}
 	
+	
+	private void waiting(int port) throws IOException{
+		boolean waiting = true;
+		try{
+			svrSocket = new ServerSocket(port);
+		}catch (IOException ioe){
+			ioe.printStackTrace();
+			System.exit(-1);
+		}	
+		while(waiting){
+			Socket userSocket = svrSocket.accept();
+			userSocket.setSoTimeout(timeout * 1000);
+			System.out.println("Client accepted " + userSocket);
+			new ChatThread(this, userSocket);
+		}
+	}
+	
+	protected void AddUser(String username, Socket socket){
+		synchronized(online){
+			online.put(username, socket);
+		}
+	}
+	
+	protected void RemoveUser(Socket socket){
+		synchronized(online){
+			online.remove(socket);
+		}
+	}
+	
+	
+	
 }
-
-//doesn't compile with javac on cs computers...
-///ChatServer.java:27: undefined reference to `ChatThread::ChatThread(java::net::Socket*)'
 
--- ChatThread.java
+++ ChatThread.java
@@ -1,51 +1,62 @@
 import java.net.*;
-//import java.util.ArrayList;
 import java.io.*;
 
 public class ChatThread extends Thread {
 	private Socket socket = null;
-	//private static ArrayList <String> users;
+	private ChatServer cs;
+	String state = "auth";
 	
-	public ChatThread(Socket svrSocket){
-		super("ChatThread");
+	public ChatThread(ChatServer server, Socket svrSocket){
+		this.cs = server;
 		this.socket = svrSocket;
+		start();
 	}
+	
 	
 	public void run(){
 		
-		try {
+		try (
 			PrintWriter serverOutput = new PrintWriter(socket.getOutputStream(), true);
-			BufferedReader clientInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			BufferedReader clientInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));){
 			
 			CommandHandler ch = new CommandHandler();
+
 			String output;
 			String input;
-			output = ch.handleInput(null);
-			serverOutput.println(output);
-			
-			while((input = clientInput.readLine()) != null){
-				serverOutput.println("Please enter a username");
-				String nameInput = clientInput.readLine();
-				if(nameInput == null){
-					return;
-				}
-				/*try{
-					synchronized(users){
-						users.add(nameInput);
-					}
-				}
-				catch(Exception e){
-					System.out.println(e);
-				}*/
+			String name;
+			if(state.equals("auth")){
+				serverOutput.println("Would you like to sign in with a name? (y/n)");
 				input = clientInput.readLine();
-				output = ch.handleInput(input);
-				serverOutput.println(output);
-				if(output.contains("+OK client signing out")){
-					break;
+				if(input.equals("y") || input.equals("Y")){
+					serverOutput.println("Please enter a username");
+					name = clientInput.readLine();
+					if(ChatServer.online.containsKey(name)){
+						serverOutput.println("Username already exists, please try again");
+						//need to test this...
+					} else{
+						serverOutput.println("Welcome " + name);
+						cs.AddUser(name, socket);
+						state = "command";
+					}
+				} else{
+					serverOutput.println("You will continue not logged in, some functions will not be available");
+					state = "command";
+				}
+			}
+			if(state.equals("command")){
+				while((input = clientInput.readLine()) != null){
+					output = ch.handleInput(input);
+					if(output.contains("+OK client signing out")){
+						serverOutput.println(output);
+						break;
+					}
+					serverOutput.println(output);
 				}
 			}
 			socket.close();	
+			System.out.println("User has logged out");
 		}
+		
 		catch(SocketTimeoutException t){
 			System.out.println("Connection has timed out");
 		}
